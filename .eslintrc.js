b304650bb75ba2035b0e968284974beb9ab01078
Inderpreet Singh <inderpreet.singh@libertymutual.com>
2018-12-27 14:19:53 -0500

    Ignore mongo _id variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,21 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-        "indent": ["error", 4, {
-            "SwitchCase": 1
-        }],
+    extends: "airbnb-base",
+    rules: {
+        indent: [
+            "error",
+            4,
+            {
+                "SwitchCase": 1
+            },
+        ],
+        "no-underscore-dangle": [
+            "error",
+            {
+                allow: [
+                    "_id"
+                ],
+            },
+        ],
     },
     env: {
         mocha: true,
